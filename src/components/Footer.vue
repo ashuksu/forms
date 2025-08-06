d710f092d844ab68f33c92492d1c917e2e491b91
Ashuksu <ashuksu@gmail.com>
2025-08-06 22:57:57 +0200

    master - updated Footer component to display the current year dynamically

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-gray-800 text-center text-sm text-white py-4 mt-8">
     <div class="container-box">
-      &copy; 2025 Footer. All rights reserved.
+      &copy; {{ new Date().getFullYear() }} Forms. All rights reserved.
     </div>
   </footer>
 </template>
