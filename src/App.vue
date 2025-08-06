6e1f8427848b14c02bd00edd329043baec0aac2f
Ashuksu <ashuksu@gmail.com>
2025-08-06 22:44:42 +0200

    master - replaced HelloWorld component with Header and Footer, updated layout structure

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo"/>
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
-    </a>
+  <div class="min-h-screen flex flex-col">
+    <Header/>
+
+    <main class="flex-1">
+      <section class="container-box py-8">
+        <h1 class="text-2xl font-bold mb-4 text-center">Home page</h1>
+        <p class="text-center">This is the main content area.</p>
+      </section>
+    </main>
+
+    <Footer/>
   </div>
-  <HelloWorld msg="Vite + Vue Tailwind CSS"/>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
