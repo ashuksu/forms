67cedcba51ddaaa58978244f689dafbdfd37ee21
Ashuksu <ashuksu@gmail.com>
2025-08-06 22:44:55 +0200

    master - created Header and Footer components with basic layout

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header class="bg-gray-800 text-white py-4">
+    <div class="container-box">
+        <h1 class="text-xl font-semibold text-right">Header</h1>
+    </div>
+  </header>
+</template>
